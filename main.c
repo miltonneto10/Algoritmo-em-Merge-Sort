e1d2d463a94c4fe4405dd041d2f1613d15254146
Milton Salustiano <109237764+miltonneto10@users.noreply.github.com>
2022-09-12 07:06:04 -0300

    Commit Inicial

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+  //ALGORITMO EM MERGE SORT
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+// funções
+void mergesort(int *v, int n){
+/* vamos apresentar suas funções.
+Dado um vetor de inteiros v e um inteiro n >= 0, ordena o vetor v[0..n-1] em ordem crescente:*/	
+	int *c = malloc(sizeof(int) * n);
+	sort(v, c, 0, n - 1);
+	free(c);
+}
+/*Dado um vetor de inteiros v e dois inteiros i e f, ordena o vetor v[i..f] em ordem crescente. O vetor c é utilizado internamente durante a ordenação:*/
+void sort(int *v, int *c, int i, int f){
+	if (i >= f)
+	return;
+	int m = (i + f) / 2;
+	sort(v, c, i, m);
+	sort(v, c, m + 1, f);
+	/* Se v[m] <= v[m + 1], então v[i..f] já está ordenado. */
+	if (v[m] <= v[m + 1])
+	return;
+merge(v, c, i, m, f);
+}
+/*Dado um vetor v e três inteiros i, m e f, sendo v[i..m] e v[m+1..f] vetores ordenados, coloca os elementos desses vetores, em ordem crescente, no vetor em v[i..f]:*/
+void merge(int *v, int *c, int i, int m, int f){
+	int z, iv = i, ic = m + 1;
+	for (z = i; z <= f; z++)
+  c[z] = v[z];
+	z = i;
+	while (iv <= m && ic <= f) {
+	  /* Invariante: v[i..z] possui os valores de v[iv..m] e v[ic..f] em ordem crescente. */
+	  if (c[iv] < c[ic])
+  v[z++] = c[iv++];
+	  else /* if (c[iv] > c[ic]) */
+  v[z++] = c[ic++];
+	}
+	while (iv <= m)
+  v[z++] = c[iv++];
+	while (ic <= f)
+  v[z++] = c[ic++];
+  }
+// programa principal
+int main (void) {
+  int i;
+  int v[8] = { -1, 7, -3, 11, 4, -2, 4, 8 };
+  mergesort(v, 8);
+  for (i = 0; i < 8; i++)
+printf("%d ", v[i]);
+  putchar('\n');
+  return 0;
+}
